7475c606a3a86f682a294885c272dea12157e05d
tomyb <tomyb@TomYb.home>
2023-01-03 17:32:05 +0100

    Un poco de el menu i un poco de la funcion jugada

--- Actividad8/src/actividad8/Actividad8.java
+++ Actividad8/src/actividad8/Actividad8.java
@@ -8,15 +8,41 @@ package actividad8;
  *
  * @author tomyb
  */
+import java.util.Scanner;
 public class Actividad8 {
 
     /**
      * @param args the command line arguments
      */
+    public static Scanner scan = new Scanner(System.in);
+    
+    public static int Jugada(int Tauler[][]){
+       int nColumna,nJugador; 
+       //demanem el numero d'usuari que esta jugant
+       System.out.print("Introdueix el numero de Jugador que ets: ");
+       nJugador=scan.nextInt();
+       //demanem el numero de columna
+       System.out.print("Introdueix el numero de columna on anira la fitxa: ");
+       nColumna=scan.nextInt();
+       
+       
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        //prueba para ver si esta bien el repositorio
+        //demanem el tamany del tauler
+        int X,Y;
+        System.out.print("Introdueix la longitud X del Tauler: ");
+        X=scan.nextInt();
+        System.out.print("Introdueix la longitud Y del Tauler: ");
+        Y=scan.nextInt();
+        //creem la matriu amb la longitud introduida per l'usuari
+        int Tauler[][]=new int[X][Y];
+        //omplim el tauler de 0
+        for(int i=0;i<X;i++){
+            for(int j=0;j<Y;j++){
+                Tauler[i][j]=0;
+            }
+        }
     }
     
 }
