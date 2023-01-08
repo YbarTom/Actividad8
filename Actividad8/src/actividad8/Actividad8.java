4acdecac57ccd18c08af9006211249767950a4d3
Mario <Mario@192.168.56.1>
2023-01-08 12:15:36 +0100

    Codigo robusto con la funcion escollirOpcio

--- Actividad8/src/actividad8/Actividad8.java
+++ Actividad8/src/actividad8/Actividad8.java
@@ -18,7 +18,7 @@ public class Actividad8 {
 	public static Scanner scan = new Scanner(System.in);
 	
 	public static int Entero(int numero) {
-            //comrpobamos que el numero introducido sea un entero, si no lo es se vuelve a pedir
+            //comprobamos que el numero introducido sea un entero, si no lo es se vuelve a pedir
 		numero = 0;
 		boolean correcte = false;
 
@@ -35,6 +35,26 @@ public class Actividad8 {
 
 		return numero;
 	}
+	
+	/**
+	 * Agafa un int entre un rang de valors
+	 * @param max int més gran
+	 * @param min int més petit
+	 * @return int que es trobi en el rang especificat
+	 */
+	static int escollirOpcio(int max, int min) {
+		int opcio = 0;
+		boolean valid = false;
+		do {
+			opcio = Entero(opcio);
+			if (opcio <= max && opcio >= min) {
+				valid = true;
+			} else {
+				System.out.println("Opcio incorrecta");
+			}
+		} while (!valid);
+		return opcio;
+	}
 
 	public static void main(String[] args) {
 		boolean repetir = true;
@@ -43,9 +63,9 @@ public class Actividad8 {
 			// demanem el tamany del tauler
 			int X=0, Y=0;
 			System.out.print("Introdueix el nombre de files del Tauler: ");
-			X = Entero(X);
+			X = escollirOpcio(100000,4);
 			System.out.print("Introdueix el nombre de columnes del Tauler: ");
-			Y = Entero(Y);
+			Y = escollirOpcio(100000,4);
 			// creem la matriu amb la longitud introduida per l'usuari
 			int Tauler[][] = new int[X][Y];
 			// omplim el tauler de 0
@@ -72,10 +92,10 @@ public class Actividad8 {
 				System.out.println("Torn del jugador " + jugador);
                                 //demanem la columna a la qual es vol posar la fixa
 				System.out.print("Introdueix la columna: ");
-				columna = Entero(columna);
+				columna = escollirOpcio(Y,1);
                                 //cridem a la funcio jugada
 				fila = ControlTauler.Jugada(Tauler, columna, jugador);
-                                //indiquem en quina fila s'ha colocat la fixa i si no s'ha pogut es torna a demanar
+                                //indiquem en quina fila s'ha colocat la fitxa i si no s'ha pogut es torna a demanar
 				if (fila >= 0) {
 					System.out.println("La fixa esta situada en la fila: " + (fila + 1));
 				} else if (fila == -1) {
@@ -90,12 +110,14 @@ public class Actividad8 {
 				}
 
 			} while (!ControlTauler.EnRatlla(Tauler, fila, columna - 1));
+			ControlTauler.Dibuixa(Tauler);
 			System.out.println("Tornar a jugar? (S/N)");
 			String resposta = scan.next();
 			if (resposta.equals("N")) {
 				repetir = false;
 			}
 		}
+		System.out.println("Fins aviat!");
 	}
 
 }
